8e5d62db7a0453970533b0b2b6a958af80085efd
jhylands <jhh521@york.ac.uk>
2018-08-14 14:43:54 +0100

    rudementory part of speach tagger

---
+++ getIngredientNouns.py
@@ -0,0 +1,28 @@
+
+from ingToPro import getStructure
+import pickle
+from wordType import wordType as wt
+
+#functions 
+def tryWord(word,wordType):
+	try:
+		return wordType(word)
+	except:
+		return 'u.'
+def tryStructure(text):
+	try:
+		return getStructure(text)
+	except:
+		return {'name':''}
+
+#gathering data
+def main():
+    with open('products.pkl','rb') as f:
+        products = pickle.load(f)
+    with open('ingredients.txt','r') as f:
+        ingredients = f.read().split('\n')
+    #main
+    wordType = wt()
+    structured_ingreients = [tryStructure(ingredient) for ingredient in ingredients]
+    w = [' '.join([tryWord(word,wordType) for word in i['name'].split(' ')]) for i in structured_ingreients]
+    return w
--- wordType.py
+++ wordType.py
@@ -1,10 +1,10 @@
 import re
 class wordType():
     def __init__(self):
-        word_capture_re = r"^([a-zA-Z]+\s+)+(—?[a-z]+\.)"
+        word_capture_re = re.compile(r"^(\D*)\d?\s\s(—?[a-z]+\.)")
         with open('words.txt' ,'r') as f:
             dictionary = f.read().split('\n')
-        matched_dic = [re.search(word_capture_re,a) for a in dictionary]
+        matched_dic = [word_capture_re.search(a) for a in dictionary]
         d = {}
         matched_dic = [a for a in matched_dic if a!=None]
         for g in matched_dic:
@@ -13,9 +13,9 @@ class wordType():
             except (AttributeError,KeyError) as e:
                 d[g.group(2)] = [g.group(1)]
             
-        inv_d = {i.strip():j for i,j in sum([[(v_,k) for v_ in v] for k, v in d.items()],[])}
+        inv_d = {i.strip().lower():j for i,j in sum([[(v_,k) for v_ in v] for k, v in d.items()],[])}
 
-        self.dictionary = d
+        self.dictionary = inv_d
 
     def __call__(self,word):
         #need to check the word here
