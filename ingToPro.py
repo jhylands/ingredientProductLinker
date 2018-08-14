5100531bc28efd26365e3e6182d59bd4d1d3bf03
jhylands <jhh521@york.ac.uk>
2018-08-14 13:47:33 +0100

    regex finished and functionalised

--- ingToPro.py
+++ ingToPro.py
@@ -1,48 +1,43 @@
 #python program to convert ingredients to products
-import re as reg
+import re 
 
 
-def match(ingreditn):
-    unit = r'(g|grams|tsp|tbsp|liters)' #match units
-    ingredient_re = reg.compile(r'''
-^\d+ #start with a digit
-{unit}
+class NotIngredient(Exception):
+    def __init__(self,text):
+        print(text)
+
+def getStructure(ingredientText):
+    #define fractional numbers
+    frac_re = "(?:[\u00BC-\u00BE]|[\u2150-\u215E])"
+
+    #define a number as 
+        #digits dot(maybe) digits(zero or more)
+        #12.5
+    #or
+        #digits fraction 
+        #1 ½
+    number_re = "(\d\s?{frac}|\d+(?:\.\d+)?)".format(frac = frac_re)
+    
+    #define what text might be considered a unit
+    unit_re = '''
+            (
+                (?:cm|g|oz|ml|l|kg|gram|kilogram|tsp|teaspoon|tbspn|tbsp|tablespoon|liter|cup|pint|fl\soz)s?
+            )
     '''
+    
+    ammount_re = "(?:{num}\s?{unit}?\s?-\s?{num}\s?{unit}?|{num}\s?{unit}?)".format(num = number_re,unit = unit_re)
 
-ingridents_re = reg.compile('^(?:(\d+)\s?((?:(?:g|ml|l|kg|gram|kilogram|tsp|teaspoon|tbsp|tablespoon|liter|cup)s?))?\s)?((?:[a-z]+\s?)+)')
-ingredients_re = reg.compile('^(?:((?:\d|[\u00BC-\u00BE]|[\u2150-\u215E])+)\s?((?:(?:g|ml|l|kg|gram|kilogram|tsp|teaspoon|tbsp|tablespoon|liter|cup|pint)s?))?\s)?((?:[a-z]+\s?)+)')
+    ammount_helpful_re = "{ammount}(?:\s?\/\s?{ammount})?".format(ammount = ammount_re)
 
-number_re = '(?:(?:\d|[\u00BC-\u00BE]|[\u2150-\u215E]|\.|-)+)'
-unit_re = '((?:(?:cm|g|oz|ml|l|kg|gram|kilogram|tsp|teaspoon|tbspn|tbsp|tablespoon|liter|cup|pint|fl oz)s?))?'
-words_re = '((?:[a-z]+\s?)+)'
-ingredients_re = reg.compile('''
-    ^ #start of string
-    ({num}) #capture the ammount
-    \s? #there may be a space between the ammount and the unit
-    ({unit})? #there may be a unit
+    word_re = "((?:[a-z]+\s?\'?)+)"
 
-    (\s?\/\s? the ammount might be represented in two units
-        ({num})
-        \s?
-        ({unit}?)
-    )?
-    \s #there is then a space
-    ({words}) #one or more words describing the item
+    ingredient_re = "(?:{help_ammount}\s)?{words}".format(help_ammount = ammount_helpful_re, words = word_re)
+    struc = re.compile(ingredient_re,re.VERBOSE)
+    
 
 
-'''.format(num=number_re,unit=unit_re,words=words_re),reg.VERBOSE)
-
-frac_re = "(?:[\u00BC-\u00BE]|[\u2150-\u215E])"
-number_re = "(?P<number>\d+\.\d+|\d\s?{frac})".format(frac = frac_re)
-unit_re = '''
-        (?:
-            (?:cm|g|oz|ml|l|kg|gram|kilogram|tsp|teaspoon|tbspn|tbsp|tablespoon|liter|cup|pint|fl oz)s?
-        )
-'''
-ammount_re = "(?:{num}\s?{unit}|{num}\s?{unit}?-{num}\s?{unit})".format(num = number_re,unit = unit_re)
-
-ammount_helpful_re = "{ammount}(?:\s?\/\s?{ammount})?".format(ammount = ammount_re)
-
-word_re = "((?:[a-z]+\s?)+)"
-
-ingredient_re = "{help_ammount}\s{words}"
+    g = struc.search(ingredientText)
+    if g is None:
+        raise NotIngredient(ingredientText)
+    
+    return {'name':g.group(13)}
